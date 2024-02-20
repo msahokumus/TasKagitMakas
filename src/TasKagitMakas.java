5b9d8d4e29d603fa50b08aa5f31e5303872a365e
Mehmetokumuss <okumusmehmet2172@gmail.com>
2024-02-20 22:55:10 +0300

    first commit

---
+++ src/TasKagitMakas.java
@@ -0,0 +1,60 @@
+import java.util.Scanner;
+import java.util.Random;
+
+public class TasKagitMakas {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+
+        String[] moves = {"taş", "kağıt", "makas"};
+
+        System.out.println("Taş kağıt makas oyununa hoş geldiniz!");
+        System.out.print("Lütfen adınızı girin: ");
+        String playerName = scanner.nextLine();
+
+        int playerScore = 0;
+        int computerScore = 0;
+
+        while (true) {
+            System.out.println("\n" + playerName + "'in Skoru: " + playerScore);
+            System.out.println("Bilgisayarın Skoru: " + computerScore);
+
+            System.out.print("Lütfen hamlenizi seçin (taş, kağıt, makas - çıkmak için q): ");
+            String playerMove = scanner.nextLine().toLowerCase();
+
+            if (playerMove.equals("q")) {
+                System.out.println("Oyundan çıkılıyor...");
+                break;
+            }
+
+            if (!playerMove.equals("taş") && !playerMove.equals("kağıt") && !playerMove.equals("makas")) {
+                System.out.println("Geçersiz hamle. Lütfen tekrar deneyin.");
+                continue;
+            }
+
+            int computerIndex = random.nextInt(3);
+            String computerMove = moves[computerIndex];
+
+            System.out.println("Bilgisayarın hamlesi: " + computerMove);
+
+            if (playerMove.equals(computerMove)) {
+                System.out.println("Berabere!");
+            } else if ((playerMove.equals("taş") && computerMove.equals("makas")) ||
+                    (playerMove.equals("kağıt") && computerMove.equals("taş")) ||
+                    (playerMove.equals("makas") && computerMove.equals("kağıt"))) {
+                System.out.println("Tebrikler, kazandınız!");
+                playerScore++;
+            } else {
+                System.out.println("Maalesef, kaybettiniz!");
+                computerScore++;
+            }
+        }
+
+        System.out.println("\nOyun sona erdi!");
+        System.out.println("Sonuçlar:");
+        System.out.println(playerName + "'in Skoru: " + playerScore);
+        System.out.println("Bilgisayarın Skoru: " + computerScore);
+
+        scanner.close();
+    }
+}
